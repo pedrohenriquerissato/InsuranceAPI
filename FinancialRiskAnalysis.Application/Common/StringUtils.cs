cda1ed977b4206652ab2f109b5fcdaecdf3da248
Pedro Henrique Rissato <pedro_giberti@hotmail.com>
2022-02-22 23:39:09 -0300

    Code cleaning

--- FinancialRiskAnalysis.Application/Common/StringUtils.cs
+++ FinancialRiskAnalysis.Application/Common/StringUtils.cs
@@ -1,5 +1,5 @@
 ﻿namespace Insurance.Application.Common
-{ 
+{
     /// <summary>
     /// Adds extensions to string property. Source: https://medium.com/@hannan.sultan1/adding-custom-naming-convention-to-the-new-system-text-json-in-net-core-c52f63f28a4f
     /// </summary>
